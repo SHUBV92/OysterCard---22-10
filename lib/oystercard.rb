3cf8f9b6e56c096f820bf4ccdf06b9efa968a17e
Ed Courage <edcourage@live.com>
2019-10-22 15:33:02 +0100

    we can now take money off card

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,16 +1,20 @@
-class OysterCard 
-attr_reader :balance
+class OysterCard
+  attr_reader :balance
 
-MAX_LIMIT = 90 
+  MAX_LIMIT = 90
 
-def initialize
-    @balance = 0
-end
-  
-def top_up(amount)
-  
-  fail "Max limit reached #{MAX_LIMIT}" if (@balance + amount) > MAX_LIMIT
-  @balance += amount
+  def initialize
+      @balance = 0
   end
+
+  def top_up(amount)
+    fail "Max limit reached #{MAX_LIMIT}" if (@balance + amount) > MAX_LIMIT
+    @balance += amount
+  end
+
+  def deduct(amount)
+    @balance -= amount
+  end
+
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,27 +1,37 @@
 require 'oystercard'
 
 describe OysterCard do
-  it 'has a balance of zero' do 
-    expect(subject.balance).to eq(0) 
-  end 
+  it 'has a balance of zero' do
+    expect(subject.balance).to eq(0)
+  end
 
 
-    describe '#top_up' do 
+  describe '#top_up' do
 
     it { is_expected.to respond_to(:top_up).with(1).argument }
 
-  it 'can top up the balance' do
-    expect{ subject.top_up 1 }.to change{ subject.balance }.by 1
-   end 
-  
-  it "to raise an error when balance reaches £90" do 
-    card = OysterCard.new
-    card.top_up(60)
-    expect { card.top_up(40) }.to raise_error("Max limit reached #{OysterCard::MAX_LIMIT}")
+    it 'can top up the balance' do
+      expect{ subject.top_up 1 }.to change{ subject.balance }.by 1
+     end
+
+    it "to raise an error when balance reaches £90" do
+      card = OysterCard.new
+      card.top_up(60)
+      expect { card.top_up(40) }.to raise_error("Max limit reached #{OysterCard::MAX_LIMIT}")
+    end
+
+  end
+
+  describe "#deduct" do
+
+    it { is_expected.to respond_to(:deduct).with(1).argument }
+
+    it 'it deducts the argument from balance' do
+      card = OysterCard.new
+      expect { card.deduct(30) }.to change { card.balance }.by -30
+    end
+
+
+
   end
 end
-end 
-
-
-
-
