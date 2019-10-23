bd60a12847facae4e1e491f4e60e004b85f39c41
liatmoss <liatmoss@hotmail.com>
2019-10-23 14:15:33 +0100

    Revert "fixed indentation"
    This reverts commit 09006c34e4ef392fced094810513e23838af4ca8.

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,19 +1,19 @@
 require 'oystercard'
 
 describe OysterCard do
-
-  context '#initialize' do
-    it "instance is created does journey equals false" do
-      expect(subject.in_journey?).to eq false
-    end
-
-    it 'has a balance of zero' do
-      expect(subject.balance).to eq(0)
-    end
-
+ 
+ context '#initialize' do
+  it "instance is created does journey equals false" do
+   expect(subject.in_journey?).to eq false  
+  end
+ 
+  it 'has a balance of zero' do
+    expect(subject.balance).to eq(0)
   end
 
-  describe '#top_up' do
+end 
+ 
+describe '#top_up' do
 
     it { is_expected.to respond_to(:top_up).with(1).argument }
 
@@ -37,37 +37,37 @@ describe OysterCard do
       card.top_up(50)
       expect { card.deduct(30) }.to change { card.balance }.by -30
     end
-  end
+  end 
 
   describe "#in_journey" do
-    it "checks if in_journey? returns true or false" do
+    it "checks if in_journey? returns true or false" do 
       card = OysterCard.new
       expect([true, false]).to include(card.in_journey?)
-    end
+    end  
   end
-
-  describe '#touch_in' do
+  
+  describe '#touch_in' do 
     it " register the card is in journey" do
       card = OysterCard.new
       card.top_up(10)
       card.touch_in
-      expect(card.in_journey?).to eq true
-    end
-  end
+      expect(card.in_journey?).to eq true 
+    end 
+  end 
 
-  it "Raise error if balance below £1" do
+  it "Raise error if balance below £1" do 
     card = OysterCard.new
     expect { card.touch_in }.to raise_error "No Entry"
-  end
+  end 
 
 
-  describe '#touch_out' do
+  describe '#touch_out' do 
     it " register the card is out of journey" do
       card = OysterCard.new
       card.touch_out
       expect(card.in_journey?).to eq false
-    end
-  end
-  
+    end 
+  end 
+
 
 end
